57dcdc65ce74310d62421fcda989174f28bd3877
b45t13n <bastds.dev@gmail.com>
2023-08-13 23:44:32 +0200

    add footer to layout

--- src/components/templates/Layout/Layout.tsx
+++ src/components/templates/Layout/Layout.tsx
@@ -2,6 +2,7 @@ import Header from "../../organisms/Header/Header";
 import './Layout.scss'
 import {Outlet} from "react-router-dom";
 import {slide as Menu} from "react-burger-menu";
+import {Footer} from "../../organisms/Footer/Footer";
 
 export default function Layout() {
     return (
@@ -15,6 +16,7 @@ export default function Layout() {
             </div>
             <Header/>
             <Outlet />
+            <Footer />
         </div>
     )
 }
