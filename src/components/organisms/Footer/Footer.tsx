87abf8a06c76584e1186d4b8c5ebfd1d380c5199
b45t13n <bastds.dev@gmail.com>
2023-08-14 00:03:03 +0200

    Add footer link

--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,12 +1,24 @@
 import "./Footer.scss"
+import {FooterLink} from "../../atoms/FooterLink/FooterLink";
+
+interface FooterLinkProps {
+    link: string,
+    innerText: string
+}
 
 export const Footer = () => {
+    const footerLinks :Array<FooterLinkProps> = [
+        {link: "/about", innerText: "A propos"},
+        {link: "/contact", innerText: "Nous contacter"},
+        {link: "/cgu", innerText: "Conditions générales d'utilisations"}
+    ]
+
     return (
         <footer>
             <div className={"footer-link"}>
-                <a href="/">A propos</a>
-                <a href="/">Nous contacter</a>
-                <a href="/">Politique de confidentialité</a>
+                {footerLinks.map((obj, key) =>
+                    <FooterLink link={obj.link} innerText={obj.innerText} key={key}/>
+                )}
             </div>
             <div className={"copyright"}>
                 <h5>Tous droits réservés - &copy; Bastien DA SILVA</h5>
