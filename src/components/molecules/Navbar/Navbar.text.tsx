e03c60f181820c6ee623db59b97505cb3d594ec4
b45t13n <bastds.dev@gmail.com>
2023-08-13 23:00:32 +0200

    setup navbar test and navbar using navlink component

--- src/components/molecules/Navbar/Navbar.text.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Navbar from './Navbar';
-
-test('renders navbar', () => {
-    render(<Navbar />);
-    const header = screen.getByText(/Hello from header/i);
-    expect(header).toBeInTheDocument();
-});
