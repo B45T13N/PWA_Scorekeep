d385655d99889781b6315ba1c00556dca516f31b
b45t13n <bastds.dev@gmail.com>
2023-08-15 17:47:37 +0200

    Beginning to setup the input and form validation

--- src/pages/Connexion/Connexion.tsx
+++ src/pages/Connexion/Connexion.tsx
@@ -1,16 +1,49 @@
 import './Connexion.scss'
+import {Input} from "../../components/atoms/Input/Input";
+import {useEffect, useState} from "react";
 function Connexion() {
+    const emailErrorMessage = "Veuillez vérifier l'email";
+
+    const [errorMessage, setErrorMessage] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+
+    const handleSubmit = (e : React.SyntheticEvent)=> {
+        e.preventDefault();
+
+        let target = e.target as typeof e.target & {
+            email: { value: string };
+            password: { value: string };
+        };
+
+        let email = target.email;
+        let password = target.password;
+
+        if(!regexEmail(email.value)) {
+            setErrorMessage(emailErrorMessage)
+        }
+
+        console.log(password.value)
+    };
+
+    useEffect(() => {
+        !regexEmail(email) ? setErrorMessage(emailErrorMessage) : setErrorMessage("")
+    }, [email])
+
+    const regexEmail = (email :string) => {
+        let emailReg = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/i);
+        return emailReg.test(email);
+    }
+
+
     return (
         <div className={"connexion-form"}>
-            <form action="">
-                <div>
-                    <label htmlFor="email">Votre email</label>
-                    <input type="text" id={"email"} />
-                </div>
-                <div>
-                    <label htmlFor="password">Votre mot de passe</label>
-                    <input type="password" id={"password"} />
-                </div>
+            <form action="" onSubmit={handleSubmit}>
+                {errorMessage && <div className="error"> {errorMessage} </div>}
+                <Input field={"email"}
+                       type={"email"}
+                       onChange={e => setEmail(e.target.value)}/>
+                <Input field={"mot de passe"}
+                       type={"password"}/>
                 <div>
                     <input type={"button"} value={"Mot de passe oublié"}/>
                     <input type={"submit"} value={"Valider"}/>
