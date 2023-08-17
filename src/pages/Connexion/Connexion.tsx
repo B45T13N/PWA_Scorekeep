3904eabbd795020ec409dc2714544439a605ae6e
b45t13n <bastds.dev@gmail.com>
2023-08-17 21:32:19 +0200

    setup some form styles

--- src/pages/Connexion/Connexion.tsx
+++ src/pages/Connexion/Connexion.tsx
@@ -2,32 +2,33 @@ import './Connexion.scss'
 import {Input} from "../../components/atoms/Input/Input";
 import {useEffect, useState} from "react";
 function Connexion() {
-    const emailErrorMessage = "Veuillez vérifier l'email";
+    const emailErrorMessage :string = "Veuillez vérifier l'email";
 
     const [errorMessage, setErrorMessage] = useState<string>('');
     const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
 
     const handleSubmit = (e : React.SyntheticEvent)=> {
         e.preventDefault();
 
-        let target = e.target as typeof e.target & {
-            email: { value: string };
-            password: { value: string };
-        };
-
-        let email = target.email;
-        let password = target.password;
-
-        if(!regexEmail(email.value)) {
+        if(!regexEmail(email)) {
             setErrorMessage(emailErrorMessage)
         }
-
-        console.log(password.value)
     };
 
-    useEffect(() => {
-        !regexEmail(email) ? setErrorMessage(emailErrorMessage) : setErrorMessage("")
-    }, [email])
+    const handleChangeInput = (inputValue: string, inputType :string) => {
+        switch (inputType) {
+            case "email":
+                !regexEmail(inputValue) ? setErrorMessage(emailErrorMessage) : setErrorMessage("");
+                setEmail(inputValue);
+                break;
+            case "password":
+                setPassword(inputValue);
+                break;
+            default:
+                console.log("Input non pris en charge");
+        }
+    }
 
     const regexEmail = (email :string) => {
         let emailReg = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/i);
@@ -38,12 +39,16 @@ function Connexion() {
     return (
         <div className={"connexion-form"}>
             <form action="" onSubmit={handleSubmit}>
-                {errorMessage && <div className="error"> {errorMessage} </div>}
+                <h3>Se connecter</h3>
+                <div className="error">
+                    {errorMessage}
+                </div>
                 <Input field={"email"}
                        type={"email"}
-                       onChange={e => setEmail(e.target.value)}/>
+                       onChange={e => handleChangeInput(e.target.value, e.target.type)}/>
                 <Input field={"mot de passe"}
-                       type={"password"}/>
+                       type={"password"}
+                       onChange={e => handleChangeInput(e.target.value, e.target.type)}/>
                 <div>
                     <input type={"button"} value={"Mot de passe oublié"}/>
                     <input type={"submit"} value={"Valider"}/>
