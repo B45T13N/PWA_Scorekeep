50b068fc5013c52c7772e63d2fce0e24e9dc8a3b
b45t13n <bastds.dev@gmail.com>
2023-08-17 21:33:24 +0200

    Add input styles and setup input animation

--- src/components/atoms/Input/Input.test.tsx
+++ src/components/atoms/Input/Input.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import {Input} from "./Input";
 
 test('renders Input', () => {
-    render(<Input type={"text"} field={"email"} />);
+    render(<Input type={"text"} field={"email"} onChange={e => console.log(e)}/>);
     const input = screen.getByTestId(/input-test/i);
     expect(input).toBeInTheDocument();
 });
--- src/components/atoms/Input/Input.tsx
+++ src/components/atoms/Input/Input.tsx
@@ -1,20 +1,42 @@
 import "./Input.scss"
-import {ChangeEventHandler} from "react";
+import {ChangeEventHandler, useState} from "react";
 
 interface InputProps {
     type: string,
     field: string,
-    onChange?: ChangeEventHandler<HTMLInputElement>
+    onChange: ChangeEventHandler<HTMLInputElement>
 }
 
 export function Input(props: InputProps) {
     const label :string = props.field.charAt(0).toUpperCase() + props.field.substring(1, props.field.length)
     const name :string = props.field === "email" ? "email" : "password";
+    const [classes, setClasses] = useState<string>("")
+    const [value, setValue] = useState<string>("")
+
+    const onFocus = ()  => {
+        setClasses("_focused")
+    }
+
+    const onBlur = ()  => {
+        setClasses("")
+    }
+
+    const onChange : ChangeEventHandler<HTMLInputElement> = (e)  => {
+        setValue(e.target.value);
+
+        return props.onChange(e)
+    }
 
     return (
-        <div className={"input-container"} data-testid={"input-test"}>
-            <label htmlFor={props.field}>{label}</label>
-            <input id={props.field} type={props.type} name={name} required onChange={props.onChange}/>
+        <div className={`input-container ${classes} ${value ? "_hasValue" : ""}`} data-testid={"input-test"}>
+            <label htmlFor={props.field} className={"floating"} aria-label={props.field}>{label}</label>
+            <input id={props.field}
+                   type={props.type}
+                   aria-required
+                   name={name} required
+                   onChange={onChange}
+                   onBlur={onBlur}
+                   onFocus={onFocus}/>
         </div>
     );
 }
