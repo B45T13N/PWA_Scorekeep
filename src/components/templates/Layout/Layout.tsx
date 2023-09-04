33cdc6e3370743036ff8eeb9f804cb9d5ef12c58
b45t13n <bastds.dev@gmail.com>
2023-09-04 12:40:44 +0200

    Change global layout style and links in the buger menu

--- src/components/templates/Layout/Layout.tsx
+++ src/components/templates/Layout/Layout.tsx
@@ -1,20 +1,20 @@
-import Header from "../../organisms/Header/Header";
 import './Layout.scss'
 import {Outlet} from "react-router-dom";
 import {slide as Menu} from "react-burger-menu";
-import {Footer} from "../../organisms/Footer/Footer";
+import Header from "../../organisms/Header/Header";
+import Footer from "../../organisms/Footer/Footer";
 
 export default function Layout() {
     return (
         <div className="page">
             <div id="slide">
                 <Menu>
-                    <a href="/" className={"active"}>Accueil</a>
-                    <a href="/" className={"active"}>Accueil</a>
-                    <a href="/" className={"active"}>Accueil</a>
+                    <a href="/">Accueil</a>
+                    <a href="/matchs">Les matchs</a>
+                    <a href="/connexion">Se connecter</a>
                 </Menu>
             </div>
-            <Header/>
+            <Header />
             <main className="content">
                 <Outlet />
             </main>
