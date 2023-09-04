05e4f9bc76db8f51dd0c5c260faaeb560f2ceea3
b45t13n <bastds.dev@gmail.com>
2023-09-04 13:29:30 +0200

    Add logout link

--- src/components/molecules/Navbar/Navbar.tsx
+++ src/components/molecules/Navbar/Navbar.tsx
@@ -1,16 +1,22 @@
 import './Navbar.scss'
 import {Navlink} from "../../atoms/Navlink/Navlink";
-interface NavlinkObject {
-    innerText: string,
-    link: string
-}
+import {useAuth} from "../../../hooks/useAuth";
+import {Logout} from "../../atoms/Logout/Logout";
+import {useEffect} from "react";
+import {NavlinkObject} from "../../interfaces/NavlinkObject";
 
 export default function Navbar() {
+    const {token} = useAuth();
+
     const navlinks :Array<NavlinkObject> = [
         {innerText: "Accueil",link: "/"},
         {innerText: "Les matchs",link: "/matchs"},
-        {innerText: "Se connecter",link: "/connexion"},
     ];
+    if(!token){
+        navlinks.push(
+            {innerText: "Se connecter",link: "/connexion"}
+        )
+    }
 
     const url :string = window.location.pathname;
 
@@ -18,9 +24,13 @@ export default function Navbar() {
          <nav data-testid={"navbar"}>
              <ul className={"link-list"}>
                  {navlinks.map((obj, key) =>
-                     // eslint-disable-next-line eqeqeq
-                    <li key={key}><Navlink link={obj.link} innerText={obj.innerText} isActive={url == obj.link}/></li>
+                    <li key={key}><Navlink link={obj.link} innerText={obj.innerText} isActive={url === obj.link}/></li>
                  )}
+                 {token &&
+                    <li>
+                        <Logout />
+                    </li>
+                 }
              </ul>
          </nav>
     )
