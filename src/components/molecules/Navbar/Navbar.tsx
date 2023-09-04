1f95e441d78c844c9e156851414fa577e82e5c33
b45t13n <bastds.dev@gmail.com>
2023-09-04 13:30:44 +0200

    Remove warning

--- src/components/molecules/Navbar/Navbar.tsx
+++ src/components/molecules/Navbar/Navbar.tsx
@@ -2,7 +2,6 @@ import './Navbar.scss'
 import {Navlink} from "../../atoms/Navlink/Navlink";
 import {useAuth} from "../../../hooks/useAuth";
 import {Logout} from "../../atoms/Logout/Logout";
-import {useEffect} from "react";
 import {NavlinkObject} from "../../interfaces/NavlinkObject";
 
 export default function Navbar() {
