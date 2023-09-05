45ee09bb5dbacca49f89e415452391e6c671f7f9
b45t13n <bastds.dev@gmail.com>
2023-09-05 13:42:34 +0200

    Begin to setup fetch match datas

--- src/pages/Dashboard/Matchs/DashboardMatchs.tsx
+++ src/pages/Dashboard/Matchs/DashboardMatchs.tsx
@@ -1,23 +1,28 @@
-import './DashboardMatchs.scss'
-import {Link} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import { Link } from 'react-router-dom';
+import useApi from '../../../hooks/useApi/useApi';
+import './DashboardMatchs.scss';
+import {Match} from "../../../interfaces/Match";
 
 export default function DashboardMatchs() {
-    const matches = [
-        { id: 1, name: 'Match 1' },
-        { id: 2, name: 'Match 2' },
-        // Add more matches as needed
-    ];
+    const apiUrl = `${process.env.REACT_APP_API_SCOREKEEP_URL}/api/games`;
+    const [matchs, setMatchs] = useState([]);
+    // Use the useApi hook to fetch data
+    const { data, error, callApi } = useApi();
 
+    useEffect(() => {
+        // Call the API when the component mounts
+        callApi(apiUrl);
+        setMatchs(Array.isArray(data) ? data : []);
+    }, [apiUrl, callApi]);
     return (
-        <article className={"dashboard-matchs"}>
+        <article className="dashboard-matchs">
             <h1>Matchs Dashboard</h1>
 
             <section className="content">
-                <div className={"add-match-button"}>
-                    <Link to={"/match/add"}>
-                        <button>
-                            Add Match
-                        </button>
+                <div className="add-match-button">
+                    <Link to="/match/add">
+                        <button>Add Match</button>
                     </Link>
                 </div>
                 <table>
@@ -30,16 +35,14 @@ export default function DashboardMatchs() {
                     </tr>
                     </thead>
                     <tbody>
-                    {matches.map((match) => (
+                    {matchs.map((match: Match) => (
                         <tr key={match.id}>
                             <td>{match.id}</td>
-                            <td>{match.name}</td>
-                            <td>{match.name}</td>
+                            <td>{match.visitorTeam.name}</td>
+                            <td>{match.category}</td>
                             <td>
                                 <Link to={`/match/edit/${match.id}`}>
-                                    <button>
-                                        Edit
-                                    </button>
+                                    <button>Edit</button>
                                 </Link>
                             </td>
                         </tr>
@@ -48,6 +51,5 @@ export default function DashboardMatchs() {
                 </table>
             </section>
         </article>
-    )
+    );
 }
-
