84d5df81578656ddde30583709445d96201fa6eb
B45T13N <bastds.dev@gmail.com>
2023-09-13 17:32:35 +0200

    Update test

--- src/pages/Dashboard/UpdateMatch/UpdateMatch.test.tsx
+++ src/pages/Dashboard/UpdateMatch/UpdateMatch.test.tsx
@@ -3,90 +3,90 @@ import {render, act, waitFor} from '@testing-library/react';
 import UpdateMatch from './UpdateMatch';
 import {BrowserRouter} from "react-router-dom";
 
-// jest.mock('react-router-dom', () => ({
-//     ...jest.requireActual('react-router-dom'),
-//     useParams: () => ({
-//         matchId: '1', // Replace with a suitable mock value
-//     }),
-// }));
-//
-// jest.mock('../../../services/apiClient', () => ({
-//     get: jest.fn(),
-//     put: jest.fn(),
-// }));
-//
-// describe('UpdateMatch Component', () => {
-//     beforeEach(() => {
-//         // Reset the mock function before each test
-//         jest.clearAllMocks();
-//     });
-//
-//     it('renders without crashing', async () => {
-//         // Mock the axios get method to return data
-//         const axios = require('../../../services/apiClient');
-//         axios.get.mockResolvedValueOnce({
-//             data: {
-//                 data: {
-//                     gameDate: '2023-09-15T10:00:00Z',
-//                     visitorTeam: 'Team A',
-//                     category: 'Category A',
-//                     address: '123 Main St/12345/City',
-//                 },
-//             },
-//         });
-//
-//         const {getByText, queryByText} =
-//             render(
-//                 <BrowserRouter>
-//                     <UpdateMatch/>
-//                 </BrowserRouter>
-//             );
-//
-//         expect(getByText(/chargement/i)).toBeInTheDocument();
-//
-//         await waitFor(() => {
-//             expect(queryByText(/chargement/i)).not.toBeInTheDocument();
-//         });
-//     });
-//
-//
-//     it('loads match data and displays form', async () => {
-//         // Mock the axios get method to return data
-//         const axios = require('../../../services/apiClient');
-//
-//         const data = {
-//             gameDate: '2023-09-15T10:00:00Z',
-//             visitorTeam: {
-//                 name: 'Team A',
-//             },
-//             category: 'Category A',
-//             address: '123 Main St/12345/City',
-//         }
-//         axios.get.mockResolvedValueOnce({
-//             data: {
-//                 data,
-//             },
-//         });
-//
-//         let getByText, getByDisplayValue;
-//
-//         await act(async () => {
-//             const component = render(
-//                 <BrowserRouter>
-//                     <UpdateMatch />
-//                 </BrowserRouter>
-//             );
-//
-//             await new Promise((resolve) => setTimeout(resolve, 0));
-//
-//             getByText = component.getByText;
-//             getByDisplayValue = component.getByDisplayValue;
-//         });
-//
-//         expect(getByText('Date du match:')).toBeInTheDocument();
-//         expect(getByDisplayValue('123 Main St')).toBeInTheDocument();
-//         expect(getByDisplayValue('12345')).toBeInTheDocument();
-//         expect(getByDisplayValue('City')).toBeInTheDocument();
-//     });
-//
-// });
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom'),
+    useParams: () => ({
+        matchId: '1', // Replace with a suitable mock value
+    }),
+}));
+
+jest.mock('../../../services/apiClient', () => ({
+    get: jest.fn(),
+    put: jest.fn(),
+}));
+
+describe('UpdateMatch Component', () => {
+    beforeEach(() => {
+        // Reset the mock function before each test
+        jest.clearAllMocks();
+    });
+
+    it('renders without crashing', async () => {
+        // Mock the axios get method to return data
+        const axios = require('../../../services/apiClient');
+        axios.get.mockResolvedValueOnce({
+            data: {
+                data: {
+                    gameDate: '2023-09-15T10:00:00Z',
+                    visitorTeam: 'Team A',
+                    category: 'Category A',
+                    address: '123 Main St/12345/City',
+                },
+            },
+        });
+
+        const {getByText, queryByText} =
+            render(
+                <BrowserRouter>
+                    <UpdateMatch/>
+                </BrowserRouter>
+            );
+
+        expect(getByText(/chargement/i)).toBeInTheDocument();
+
+        await waitFor(() => {
+            expect(queryByText(/chargement/i)).not.toBeInTheDocument();
+        });
+    });
+
+
+    it('loads match data and displays form', async () => {
+        // Mock the axios get method to return data
+        const axios = require('../../../services/apiClient');
+
+        const data = {
+            gameDate: '2023-09-15T10:00:00Z',
+            visitorTeam: {
+                name: 'Team A',
+            },
+            category: 'Category A',
+            address: '123 Main St/12345/City',
+        }
+        axios.get.mockResolvedValueOnce({
+            data: {
+                data,
+            },
+        });
+
+        let getByText, getByDisplayValue;
+
+        await act(async () => {
+            const component = render(
+                <BrowserRouter>
+                    <UpdateMatch />
+                </BrowserRouter>
+            );
+
+            await new Promise((resolve) => setTimeout(resolve, 0));
+
+            getByText = component.getByText;
+            getByDisplayValue = component.getByDisplayValue;
+        });
+
+        expect(getByText('Date du match:')).toBeInTheDocument();
+        expect(getByDisplayValue('123 Main St')).toBeInTheDocument();
+        expect(getByDisplayValue('12345')).toBeInTheDocument();
+        expect(getByDisplayValue('City')).toBeInTheDocument();
+    });
+
+});
