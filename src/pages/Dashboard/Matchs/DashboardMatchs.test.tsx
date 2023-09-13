536cffa755d6a4b215627376811f94d5842830e0
b45t13n <bastds.dev@gmail.com>
2023-09-13 09:46:47 +0200

    Setup dashboard test

--- src/pages/Dashboard/Matchs/DashboardMatchs.test.tsx
+++ src/pages/Dashboard/Matchs/DashboardMatchs.test.tsx
@@ -1,23 +1,95 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'; // Import for expect assertions
 import DashboardMatchs from './DashboardMatchs';
 import {BrowserRouter} from "react-router-dom";
 
-jest.mock("../../../hooks/useAuth", () => ({
+
+jest.mock("../../../hooks/useAuth/useAuth", () => ({
     useAuth: jest.fn(),
 }));
 
-test('renders h1', () => {
-    const mockUseAuth = {
-        logout: () => {}
-    };
-    jest.requireMock("../../../hooks/useAuth").useAuth.mockReturnValue(mockUseAuth);
+jest.mock('../../../hooks/useApi/useApi', () => ({
+    __esModule: true,
+    default: jest.fn(),
+}));
 
-    render(
-        <BrowserRouter>
-            <DashboardMatchs />
-        </BrowserRouter>
-    );
-    const h1 = screen.getByText(/Matchs Dashboard/i);
-    expect(h1).toBeInTheDocument();
+describe('DashboardMatchs', () => {
+    beforeEach(() => {
+        // Reset the mock function before each test
+        jest.clearAllMocks();
+    });
+
+    it('renders the component with loading state', () => {
+        jest.requireMock('../../../hooks/useApi/useApi').default.mockReturnValue({
+            data: [],
+            error: null,
+            isLoading: true,
+            callApi: jest.fn(),
+        });
+        const mockUseAuth = {
+            logout: () => {}
+        };
+        jest.requireMock("../../../hooks/useAuth/useAuth").useAuth.mockReturnValue(mockUseAuth);
+
+        render(
+            <BrowserRouter>
+                <DashboardMatchs />
+            </BrowserRouter>);
+
+        // Assert that loading text is displayed
+        expect(screen.getByText('Matchs Dashboard')).toBeInTheDocument();
+    });
+
+    it('renders the component with error state', () => {
+        jest.requireMock('../../../hooks/useApi/useApi').default.mockReturnValue({
+            data: [],
+            error: 'Error message',
+            isLoading: false,
+            callApi: jest.fn(),
+        });
+        const mockUseAuth = {
+            logout: () => {}
+        };
+        jest.requireMock("../../../hooks/useAuth/useAuth").useAuth.mockReturnValue(mockUseAuth);
+
+        render(
+            <BrowserRouter>
+                <DashboardMatchs />
+            </BrowserRouter>
+        );
+
+        expect(screen.getByText('Erreur lors de la récupération des matchs')).toBeInTheDocument();
+    });
+
+    it('renders the component with data', () => {
+        const mockData = [
+            {
+                id: 1,
+                gameDate: '2023-09-15T12:00:00Z',
+                visitorTeam: { name: 'Visitor Team' },
+                category: 'Category A',
+            },
+        ];
+        jest.requireMock('../../../hooks/useApi/useApi').default.mockReturnValue({
+            data: mockData,
+            error: null,
+            isLoading: false,
+            callApi: jest.fn(),
+        });
+        const mockUseAuth = {
+            logout: () => {}
+        };
+        jest.requireMock("../../../hooks/useAuth/useAuth").useAuth.mockReturnValue(mockUseAuth);
+        render(
+            <BrowserRouter>
+                <DashboardMatchs />
+            </BrowserRouter>
+        );
+
+        expect(screen.getByText('Matchs Dashboard')).toBeInTheDocument();
+        expect(screen.getByText('15/09/2023 14:00')).toBeInTheDocument();
+        expect(screen.getByText('Visitor Team')).toBeInTheDocument();
+        expect(screen.getByText('Category A')).toBeInTheDocument();
+    });
 });
