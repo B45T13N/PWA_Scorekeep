c90def4414b286546c94fee9aab67b6292d2ad36
B45T13N <bastds.dev@gmail.com>
2023-09-14 15:31:15 +0200

    Update test after update the home text

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Home from './Home';
 
-test('renders h1', () => {
+test('renders h2', () => {
     render(<Home />);
-    const h1 = screen.getByText(/Hello from home/i);
-    expect(h1).toBeInTheDocument();
+    const h2 = screen.getByText(/Suivez, enregistrez et gérez vos Matchs avec Scorekeep/i);
+    expect(h2).toBeInTheDocument();
 });
