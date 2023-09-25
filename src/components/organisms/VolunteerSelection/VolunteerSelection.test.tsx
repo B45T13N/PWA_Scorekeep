827dcb7f2998a3ebceb78203291f66ccc11d50a1
B45T13N <bastds.dev@gmail.com>
2023-09-25 16:01:20 +0200

    Update tests

--- src/components/organisms/VolunteerSelection/VolunteerSelection.test.tsx
+++ src/components/organisms/VolunteerSelection/VolunteerSelection.test.tsx
@@ -1,16 +1,109 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import VolunteerSelection from "./VolunteerSelection";
+import { render, fireEvent, screen } from '@testing-library/react';
+import VolunteerSelection from './VolunteerSelection';
+import Volunteers from "../../../pages/Dashboard/Volunteers/Volunteers";
+import apiClient from "../../../services/apiClient";
 
-jest.mock("../../../hooks/useAuth/useAuth", () => ({
-    useAuth: jest.fn(),
+
+jest.mock('../../../services/apiClient', () => ({
+    get: jest.fn(),
+    put: jest.fn(),
 }));
 
-test('renders ', () => {
-    const mockUseAuth = {
-        logout: () => {}
+describe('VolunteerSelection', () => {
+    const mockProps = {
+        matchId: 1,
+        matchCategory: 'Category',
+        visitorTeamName: 'Visitor Team',
+        matchDate: new Date(),
+        isHomeMatch: false,
+        timekeepers: [{id: 1, name: ""}],
+        roomManagers: [{id: 1, name: ""}],
+        secretaries: [{id: 1, name: ""}],
+        timekeeperId: 2,
+        secretaryId: 3,
+        roomManagerId: 4,
     };
-    jest.requireMock("../../../hooks/useAuth/useAuth").useAuth.mockReturnValue(mockUseAuth);
-    const h2 = screen.getByText(/Scorekeep/i);
-    expect(h2).toBeInTheDocument();
+
+    it('renders without crashing', () => {
+        render(
+            <table>
+                <tbody>
+                <VolunteerSelection {...mockProps} />
+                </tbody>
+            </table>
+        );
+    });
+
+    it('handles radio button selection for timekeepers', () => {
+        render(
+            <table>
+                <tbody>
+                <VolunteerSelection {...mockProps} />
+                </tbody>
+            </table>
+        );
+
+        const timekeeperRadioButton = screen.getByLabelText(mockProps.timekeepers[0].name);
+        fireEvent.click(timekeeperRadioButton);
+
+        expect(timekeeperRadioButton).toBeChecked();
+    });
+
+    it('handles radio button selection for secretaries', () => {
+        render(
+            <table>
+                <tbody>
+                <VolunteerSelection {...mockProps} />
+                </tbody>
+            </table>
+        );
+
+        const secretaryRadioButton = screen.getByLabelText(mockProps.secretaries[0].name);
+        fireEvent.click(secretaryRadioButton);
+
+        expect(secretaryRadioButton).toBeChecked();
+    });
+
+    it('handles radio button selection for room managers', () => {
+        render(
+            <table>
+                <tbody>
+                <VolunteerSelection {...mockProps} />
+                </tbody>
+            </table>
+        );
+
+        const roomManagerRadioButton = screen.getByLabelText(mockProps.roomManagers[0].name);
+        fireEvent.click(roomManagerRadioButton);
+
+        expect(roomManagerRadioButton).toBeChecked();
+    });
+
+    it('calls the handleClickButton function when "Mettre à jour" button is clicked', async () => {
+        render(
+            <table>
+                <tbody>
+                <VolunteerSelection {...mockProps} />
+                </tbody>
+            </table>);
+        const updateButton = screen.getByText('Mettre à jour');
+        const putMock = jest.spyOn(apiClient, 'put');
+        const logSpy = jest.spyOn(global.console, 'log');
+        putMock.mockResolvedValue({status: 200});
+
+        fireEvent.click(updateButton);
+
+        await Promise.resolve();
+
+        expect(putMock).toHaveBeenCalledWith(
+            `/api/games/addVolunteers/${mockProps.matchId}`,
+            {
+                timekeeperId: `${mockProps.timekeeperId}`,
+                secretaryId: `${mockProps.secretaryId}`,
+                roomManagerId: `${mockProps.roomManagerId}`,
+            }
+        );
+        expect(logSpy).toHaveBeenCalledWith('Match updated successfully');
+    });
 });
