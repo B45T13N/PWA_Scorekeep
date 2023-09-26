84189c637bf6e32a7c6c12e74593dffcf0e0e6a1
B45T13N <bastds.dev@gmail.com>
2023-09-26 15:09:09 +0200

    Remove warnings

--- src/pages/Matchs/Matchs.test.tsx
+++ src/pages/Matchs/Matchs.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {render, screen, waitFor} from '@testing-library/react';
 import Matchs from "./Matchs";
-import axios from "../../services/apiClient";
 
 jest.mock('../../services/apiClient', () => ({
     get: jest.fn(),
