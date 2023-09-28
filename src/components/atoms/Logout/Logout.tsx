c67f707103a375c62da5395eaec5f456ad2bc00e
B45T13N <bastds.dev@gmail.com>
2023-09-28 14:22:24 +0200

    Remove warnings

--- src/components/atoms/Logout/Logout.tsx
+++ src/components/atoms/Logout/Logout.tsx
@@ -6,6 +6,7 @@ export const Logout = () => {
     const {logout} = useAuth();
 
     return (
+        // eslint-disable-next-line jsx-a11y/anchor-is-valid
         <a href="#" about="Lien de navigation pour se déconnecter" className={`navlink`} onClick={logout}>
             Se déconnecter
         </a>
