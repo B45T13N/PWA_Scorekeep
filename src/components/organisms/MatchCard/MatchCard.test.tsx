0440b501da8809383bd9b756b8fac014302ac4fa
B45T13N <bastds.dev@gmail.com>
2023-09-28 13:52:01 +0200

    Reformat code and add gameDate

--- src/components/organisms/MatchCard/MatchCard.test.tsx
+++ src/components/organisms/MatchCard/MatchCard.test.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import MatchCard from "./MatchCard";
 
+jest.mock('../../../services/apiClient', () => ({
+    get: jest.fn(),
+    put: jest.fn(),
+}));
+
 describe("RadioInputComponent", () => {
     test('renders radio input', () => {
 
-        render(<MatchCard date={new Date()} visitorTeamName={"Visitor Team"} category={"Junior"} isHomeMatch={true}/>);
+        render(<MatchCard gameDate={new Date()}
+                          visitorTeamName={"Visitor Team"}
+                          category={"Junior"}
+                          isHomeMatch={true}
+                          gameId={"1"}/>);
         const teamName = screen.getByText(/Visitor Team/i);
         const category = screen.getByText(/Junior/i);
         expect(teamName).toBeInTheDocument();
