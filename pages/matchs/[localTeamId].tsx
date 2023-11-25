eebe430c9f597441c2db8f1418ca60a78737f477
B45T13N <bastds.dev@gmail.com>
2023-11-25 15:27:08 +0100

    setup auth with next

---
+++ pages/matchs/[localTeamId].tsx
@@ -0,0 +1,56 @@
+import './matchs.scss'
+import React, {useEffect, useState} from "react";
+import apiClient from "@/services/apiClient";
+import {Match} from "@/interfaces/Match";
+import MatchCard from "@/components/organisms/MatchCard/MatchCard";
+import {useRouter} from "next/router";
+import * as process from "process";
+
+export default function Matchs() {
+    const router = useRouter();
+    const { localTeamId } = router.query;
+
+    const apiUrl = `${process.env.NEXT_PUBLIC_API_SCOREKEEP_URL}/api/weekGames?local_team_id=${localTeamId}`;
+    const [error, setError] = useState<boolean>(false);
+    const [data, setData] = useState([]);
+
+
+    useEffect(() => {
+        apiClient.get(apiUrl)
+            .then((result) =>{
+                setData(result.data.data)
+            })
+            .catch((error) => {
+                setError(true);
+                console.log("Erreur lors de la récupération des matchs");
+            })
+    }, [apiUrl]);
+
+
+
+    return (
+        <article className="matchs-content">
+            <h2>Les matchs</h2>
+            {data.length === 0 ? (
+                    error ?
+                        <p>Erreur lors de la récupération des données</p>
+                        :
+                        <p>Pas de matchs disponibles</p>
+                )
+                : (
+                    <section className={"matchs-display"}>
+                        {data.map((match: Match) => (
+                            <MatchCard
+                                key={match.id}
+                                visitorTeamName={match.visitorTeam.name}
+                                category={match.category}
+                                gameDate={match.gameDate}
+                                isHomeMatch={match.isHomeMatch}
+                                gameId={match.id.toString()}
+                            />
+                        ))}
+                    </section>
+                )}
+        </article>
+    )
+}
