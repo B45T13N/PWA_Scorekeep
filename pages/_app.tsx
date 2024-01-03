91df6cf8fc0c3b947116fe59a6004d20145bfc98
B45T13N <bastds.dev@gmail.com>
2024-01-03 20:35:59 +0100

    Setup add match pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,9 @@ import Layout from "../app/layout";
 import '../styles/global.scss';
 import {getCSRFToken} from "@/services/apiClient";
 import {useEffect} from "react";
-import AuthMiddleware from "@/app/middleware/authMiddleware";
+import {registerLocale} from "react-datepicker";
+import fr from "date-fns/locale/fr";
+registerLocale("fr", fr);
 
 export default function MyApp({ Component, pageProps }: AppProps) {
     useEffect(() => {
